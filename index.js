a4eb4c8ee74f46cfab8ce9b48db724feeacdc929
elektrycznyy <wyszatycki.m@gmail.com>
2022-04-25 22:53:44 +0200

    init

---
+++ index.js
@@ -0,0 +1,197 @@
+// Require the libraries:
+var SocketIOFileUpload = require("socketio-file-upload")
+const fs = require('fs')
+
+const express = require('express')
+const app = express()
+app.use(SocketIOFileUpload.router);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const http = require('http')
+const { Server } = require('socket.io');
+const { join } = require("path");
+const server = http.createServer(app)
+const io = new Server(server)
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/uploads'));
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+var user = {}
+var rooms = ['DefaultRoom']
+
+
+app.get('/', (req, res) => {
+
+  res.render('index')
+})
+
+app.get('/game', (req, res) => {
+
+  res.render('game');
+})
+
+app.post('/game', (req, res) => {
+  // JAK PRZESŁAĆ ROOMS DO KLIENTA
+
+  var username = req.body.username;
+
+  res.render('game', { roomlist: rooms, user: username });
+})
+
+// app.get('/deleteimage', (req, res) => {
+//     console.log(req.query);
+//     res.json(req.query.path);
+//     fs.unlinkSync(__dirname + "/uploads/" + req.query.path, () => {
+
+//     })
+// })
+let unmatched;
+players = {}, unmatched;
+
+
+
+
+io.on("connection", function (socket) {
+
+  joinServer(socket);
+
+
+  console.log("New socket connected: " + socket.id)
+
+  socket.on('join', (data) => {
+    var room = data.room;
+    var username = data.username;
+
+    socket.join(room);
+
+    io.to(room).emit('system-message', { 'msg': username + " has joined the " + room })
+
+  })
+
+  socket.on('get-player-count', async (data) => {
+    const clients = await io.in(data.room).fetchSockets();
+    console.log("Ilosc socketow w pokoju: " + clients.length)
+    socket.emit('get-player-count-response', { 'clients': clients.length, 'room': data.room })
+  })
+
+
+  socket.on('leave', (data) => {
+    console.log(data)
+    var room = data.room;
+    var username = data.username;
+    io.to(room).emit('system-message', { 'msg': username + " has left the " + room })
+    socket.leave(room);
+  })
+
+
+  // Make an instance of SocketIOFileUpload and listen on this socket:
+
+
+  socket.on('new-user', data => {
+    user[socket.id] = data.name
+    room = data.room
+    socket.to(room).emit('user-connected', data)
+
+
+    console.log("User info: " + user[socket.id])
+
+  })
+
+  socket.on('send-chat-message', data => {
+    socket.to(data.room).emit('chat-message', { message: data.message, name: user[socket.id] })
+  })
+
+  socket.on('send-video', path => {
+    socket.to(path.room).emit('display-video', path)
+  })
+
+  var uploader = new SocketIOFileUpload();
+  uploader.dir = "uploads";
+  uploader.listen(socket);
+
+  // Do something when a file is saved:
+  uploader.on("saved", function (event) {
+    event.file.clientDetail.name = event.file.name;
+  });
+
+  // Error handler:
+  uploader.on("error", function (event) {
+    console.log("Error from uploader", event);
+  });
+
+  socket.on('create-room', (data) => {
+    if (rooms.indexOf(data.new_room_name) !== -1) {
+      console.log("The room already exsitstst")
+    } else {
+      console.log(data)
+      let room_name = data.new_room_name
+      rooms.push(room_name)
+      console.log("LIST OF ROOMS: " + rooms)
+      io.emit('new-room-received', data)
+    }
+  })
+
+socket.on('upload-pass-turn', (data) => {
+  socket.broadcast.emit('upload-pass-turn-response', data)
+})
+
+});
+
+function joinServer(socket) {
+  // Add the player to our object of players
+  players[socket.id] = {
+    // The opponent will either be the socket that is
+    // currently unmatched, or it will be null if no
+    // players are unmatched
+    opponent: unmatched,
+    // The symbol will become 'O' if the player is unmatched
+    isTaskGiver: true,
+    isPlayerTurn: true,
+    canUpload: true,
+    // The socket that is associated with this player
+    socket: socket
+  };
+  // Every other player is marked as 'unmatched', which means
+  // there is not another player to pair them with yet. As soon
+  // as the next socket joins, the unmatched player is paired with
+  // the new socket and the unmatched variable is set back to null
+
+  if (unmatched) {
+    players[socket.id].isTaskGiver = false;
+    players[socket.id].isPlayerTurn = false;
+    players[socket.id].canUpload = false;
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
+
+  var player_info = {
+    'socket': socket.id,
+    'opponent': players[socket.id].opponent,
+    'task_giver': players[socket.id].isTaskGiver,
+    'player_turn': players[socket.id].isPlayerTurn,
+    'can_upload': players[socket.id].canUpload
+  }
+
+  console.log("Player info: \n" + JSON.stringify(player_info))
+  socket.emit('player-info', player_info)
+}
+
+function getOpponent(socket) {
+  if (!players[socket.id].opponent) {
+    return;
+  }
+  return players[players[socket.id].opponent].socket;
+}
+
+
+
+
+server.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});
